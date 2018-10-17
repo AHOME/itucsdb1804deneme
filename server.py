a11c4de800cfea019161cda3f75f9d4a765757f6
yilmazmuhammed <yilmazmu15@itu.edu.tr>
2018-10-17 18:01:55 +0300

    books page

--- server.py
+++ server.py
@@ -7,6 +7,10 @@ app = Flask(__name__)
 def home_page():
     return render_template("home.html")
 
+@app.route("/books")
+def books_page():
+	return render_template("books.html")
+
 
 if __name__ == "__main__":
     app.run()

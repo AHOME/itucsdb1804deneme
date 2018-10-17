5ebedd73ccfb87881e8dfd75ed293cabc251ad86
yilmazmuhammed <yilmazmu15@itu.edu.tr>
2018-10-17 18:12:36 +0300

    merge server.py

--- server.py
+++ server.py
@@ -1,15 +1,16 @@
-from flask import Flask
-import views
+from flask import Flask, render_template
 
-def create_app():
-    app = Flask(__name__)
+app = Flask(__name__)
 
-    app.add_url_rule("/", view_func=views.home_page)
-    app.add_url_rule("/books", view_func=views.books_page)
 
-    return app
+@app.route("/")
+def home_page():
+    return render_template("home.html")
+
+@app.route("/books")
+def books_page():
+	return render_template("books.html")
 
 
 if __name__ == "__main__":
-    app = create_app()
-    app.run()
+app.run()

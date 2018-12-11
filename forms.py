b23239859f35d7815d754fc3c1d48749357e77d7
AHOME <ahmedyasinkul@hotmail.com>
2018-12-11 22:32:56 +0300

    Forms.py updated

--- forms.py
+++ forms.py
@@ -17,8 +17,13 @@ class PersonForm(FlaskForm):
     p_nationality = StringField("Person nationality", validators=[Optional(), Length(1, 50, "Nationality length should be between %(min)d and %(max)d character")], id='person_nationality')
 
 
-
 class AuthorForm(PersonForm):
     a_biography = TextAreaField("Author biography", validators=[Optional(), Length(1, 1000, "Author biography length should be between %(min)d and %(max)d character")], id='author_biography')
     submit = SubmitField("Add Author")
 
+
+class SignUpForm(PersonForm):
+    c_username = StringField("Username", validators=[InputRequired("Please enter your username"), Length(5, 30, "Username length must between %(min)d and %(max)d character")], id='customer_username')
+    c_email = StringField("E-mail", validators=[Email("You must provide a valid mail address"), Length(3, 50, "Email can not be longer than %(max)d character")], id='customer_email')
+    c_password = PasswordField("Password", validators=[InputRequired("Please enter your password"), Length(6, 20, "Password length must between %(min)d and %(max)d character")], id='customer_password')
+    c_phone = StringField("Customer phone", validators=[InputRequired("Please enter your phone number"), Length(8, 10, "Phone number can not be longer than %(max)d length")], id='customer_phone')

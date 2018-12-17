c694834aa1eeb2095bbc0c00c8c41b76b69c9dd1
AHOME <ahmedyasinkul@hotmail.com>
2018-12-17 06:10:53 +0300

    Fix for signUp page

--- forms.py
+++ forms.py
@@ -24,10 +24,10 @@ class AuthorForm(PersonForm):
 
 
 class SignUpForm(PersonForm):
-    c_username = StringField("Username", validators=[InputRequired("Please enter your username"), Length(5, 30, "Username length must between %(min)d and %(max)d character")], id='customer_username')
+    c_username = StringField("Username", validators=[InputRequired("Please enter your username"), Length(5, 30, "Username length must be between %(min)d and %(max)d character")], id='customer_username')
     c_email = StringField("E-mail", validators=[Email("You must provide a valid mail address"), Length(3, 50, "Email can not be longer than %(max)d character")], id='customer_email')
-    c_password = PasswordField("Password", validators=[InputRequired("Please enter your password"), Length(6, 20, "Password length must between %(min)d and %(max)d character")], id='customer_password')
-    c_phone = StringField("Customer phone", validators=[InputRequired("Please enter your phone number"), Length(8, 10, "Phone number can not be longer than %(max)d length")], id='customer_phone')
+    c_password = PasswordField("Password", validators=[InputRequired("Please enter your password"), Length(6, 20, "Password length must be between %(min)d and %(max)d character")], id='customer_password')
+    c_phone = StringField("Customer phone", validators=[InputRequired("Please enter your phone number"), Length(8, 10, "Phone number must be between %(min)d and %(max)d length")], id='customer_phone')
     submit = SubmitField("Sign up")
 
 
